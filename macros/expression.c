82585cab0e2a72403a9ec355dadbad27fc6a2aee
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-11-26 05:23:32 +0500

    macros

--- macros/expression.c
+++ macros/expression.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
-#define cal(a,b) ( (a*a) +  (b*b) )
+#define cal(a,b) ( (a*a) +  (b*b) ) + a
 int main(){
 
+    #ifdef cal
 int x = 5, y = 10;
 
 int result = cal(x,y);
 
 printf("\nResult : %d",result);
 
+#endif
 }
